1444b2db9a119be9938761cbf08458fd37b7433b
JustinParviz <jarshravan@gmail.com>
2024-08-12 21:15:16 -0400

    Switched to the "Everything" endpoint to get articles with images and used the query parameter to create categories

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const searchButton = document.getElementById("search-button");
 
 
 async function fetchTopNews(category) {
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=20&apiKey=${apiKey}`;
+    const apiUrl = `https://newsapi.org/v2/everything?q=${category}&pageSize=20&sortBy=popularity&apiKey=${apiKey}`;
+    
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
         if (data.status !== "ok") {
             console.error(`Error fetching top news by category ${category}:`, data);
             return [];
-        }
+        } 
         return data.articles;
     } catch (error) {
         console.error(`Error fetching top news by category ${category}`, error);
@@ -24,14 +25,17 @@ async function fetchTopNews(category) {
 async function fetchAllTopNews() {
     const categories = ["business", "entertainment", "health", "science", "sports", "technology"];
     const allNews = {};
+
     for (const category of categories) {
         const articles = await fetchTopNews(category);
+        console.log(articles, "***");
         const articlesWithImages = articles.filter(article => article.urlToImage !== null);
+        console.log(articlesWithImages, "!!");
         allNews[category] = articlesWithImages;
     }
+
     return allNews;
 }
-
 
 // fetchAllTopNews().then(allNews => {
 //     console.log("All top news by category:", allNews);
@@ -70,27 +74,34 @@ async function fetchNewsQuery(query, pageSize = 20) {
 
 function displayBlogs(allNews) {
     blogContainer.innerHTML = "";
+
     const articlesWithImages = Object.values(allNews).flat();
+
     articlesWithImages.forEach((article) => {
         const blogCard = document.createElement("div");
         blogCard.classList.add("blog-card");
+
         const img = document.createElement("img");
         img.src = article.urlToImage;
         img.alt = article.title;
+
         const title = document.createElement("h2");
         title.textContent = article.title;
+
         const description = document.createElement("p");
         description.textContent = article.description;
+
         blogCard.appendChild(img);
         blogCard.appendChild(title);
         blogCard.appendChild(description);
+
         blogCard.addEventListener("click", () => {
             window.open(article.url, "_blank");
         });
+        
         blogContainer.appendChild(blogCard);
     });
 }
-
 
 (async () => {
     try {
@@ -101,4 +112,5 @@ function displayBlogs(allNews) {
         console.error("Error fetching all top news", error);
     }
 })();
+
 
